34d0a7e82b4fd2f76b4900575a34d103b1c0d6cf
3090 <my_server@edu.cn>
2023-01-27 22:06:05 +0800

    feat(fonts): change fonts to hack from saurcepro

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font Mono:pixelsize=30:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:pixelsize=30:antialias=true:autohint=true";
 static int borderpx = 4;
 
 /*
